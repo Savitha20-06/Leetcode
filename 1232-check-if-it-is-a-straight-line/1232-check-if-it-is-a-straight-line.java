ca9ae8893da73deefeab62d82b9d42a5178e8211
Savitha20-06 <savithasubbusamy20@gmail.com>
2026-07-29 12:32:55 +0530

    Time: 0 ms (100%), Space: 45.2 MB (24.06%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public boolean checkStraightLine(int[][] c) 
+    {
+        int x1=c[0][0];
+        int y1=c[0][1];
+        int x2=c[1][0];
+        int y2=c[1][1];
+        for(int i=2;i<c.length;i++)
+        {
+            int x=c[i][0];
+            int y=c[i][1];
+            if((y-y1)*(x2-x1)!=(y2-y1)*(x-x1))
+            return false;
+        }
+        return true;
+    }
+}
