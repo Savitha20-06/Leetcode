61f7a92a55423d023e88d433968ad5fe0a6b6d1e
Savitha20-06 <savithasubbusamy20@gmail.com>
2026-09-02 12:01:07 +0530

    Time: 2 ms (99.8%), Space: 45.5 MB (82.36%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,69 +1,45 @@
-// class Solution 
-// {
-//     public String minWindow(String s, String t) 
-//     {
-
-//     }
-// }
 class Solution 
 {
     public String minWindow(String s, String t) 
     {
-        int[] need = new int[128];
-
-        for(int i = 0; i < t.length(); i++)
+        int[] need=new int[128];
+        for(int i=0;i<t.length();i++)
         {
             need[t.charAt(i)]++;
         }
-
         int left = 0;
         int count = 0;
-
-        int min = Integer.MAX_VALUE;
-        int start = 0;
-
-        for(int right = 0; right < s.length(); right++)
+        int min=Integer.MAX_VALUE;
+        int start=0;
+        for(int right=0;right<s.length();right++)
         {
-            char c = s.charAt(right);
-
-            // Character is required
-            if(need[c] > 0)
+            char c=s.charAt(right);
+            if(need[c]>0)
             {
                 count++;
             }
-
             need[c]--;
-
-            // Window is valid
-            while(count == t.length())
+            while(count==t.length())
             {
-                int len = right - left + 1;
-
-                if(len < min)
+                int len=right-left+1;
+                if(len<min)
                 {
-                    min = len;
-                    start = left;
+                    min=len;
+                    start=left;
                 }
-
-                char ch = s.charAt(left);
-
+                char ch=s.charAt(left);
                 need[ch]++;
-
-                // We removed a required character
-                if(need[ch] > 0)
+                if(need[ch]>0)
                 {
                     count--;
                 }
-
                 left++;
             }
         }
-
-        if(min == Integer.MAX_VALUE)
+        if(min==Integer.MAX_VALUE)
         {
             return "";
         }
-
-        return s.substring(start, start + min);
+        return s.substring(start,start + min);
     }
 }
